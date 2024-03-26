498eb79fdeb5c1a4945b6af8053448741cbf5e58
The Fisher <114557710+RadomirSz@users.noreply.github.com>
2024-03-26 10:29:35 +0100

    Add files via upload

---
+++ Programowanie Obiektowe/Klasa 2/SamochodyRS/SamochodyRS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("SamochodyRS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SamochodyRS")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("6656b816-098d-47f3-a439-1232e7ec7916")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Programowanie Obiektowe/Klasa 2/SamochodyRS/SamochodyRS/Properties/Samochod.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamochodyRS.Properties
+{
+    enum TypPaliwa
+    {
+        Benzyna, 
+        Diesel, 
+        Elektryczny, 
+        Hybrydowy
+    }
+    internal class Samochod
+    {
+        static int IloscKol = 4;
+        //Statyczne pole IloscKol: Wspólne dla wszystkich obiektów, ustawione na wartość 4.
+
+        private string marka;
+        private string model;
+        private int rokProdukcji;
+        //Prywatne pola: marka, model(typu string) oraz rokProdukcji(typu int).
+
+        private DateTime DataPierwszejRejestracji;
+        //Właściwość DataPierwszejRejestracji: Typu DateTime, przechowuje datę pierwszej rejestracji samochodu.
+
+        private float PojemnoscSilnika;
+
+        // Domyślny konstruktor: Nie przyjmuje argumentów, ustawia marka i model na “Nieznany”, rokProdukcji na 0.
+        public Samochod()
+        {
+            marka = "Nieznany";
+            model = "Nieznany";
+            rokProdukcji = 0;
+        }
+        //Parametryczny konstruktor: Przyjmuje argumenty dla wszystkich pól i ustawia je odpowiednio.
+        public Samochod(string marka, string model, int rokProdukcji, DateTime DataPierwszejRejestracji, float PojemnoscSilnika)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.rokProdukcji = rokProdukcji;
+            this.DataPierwszejRejestracji = DataPierwszejRejestracji;
+            this.PojemnoscSilnika = PojemnoscSilnika;
+        }
+
+
+        // Metoda WyswietlInformacje: Wyświetla informacje o samochodzie.
+
+        public void WyswietlInformacje()
+        {
+            Console.WriteLine($"Samochód marki: {marka}, model: {model}, o pojemności silnika {PojemnoscSilnika}, wyprodukowany w {rokProdukcji} a zarejestrowany {DataPierwszejRejestracji}.");
+        }
+        //Metoda ObliczWiek: Oblicza wiek samochodu.
+        public int ObliczWiek()
+        {
+            return DateTime.Now.Year - rokProdukcji;
+        }
+        //Metoda ZmienMarkeIModel: Zmienia markę i model samochodu.
+        public void ZmienMarkeIModel(string marka, string model)
+        {
+            this.marka = marka;
+            this.model = model;
+        }
+        //Metoda CzyKlasyk: Sprawdza, czy wybrany samochód jest klasykiem.
+        public bool czyKlasyk()
+        {
+            if (DateTime.Now.Year - rokProdukcji >= 25) return true;
+            return false;
+        }
+        //Metoda WyswietlInformacjeJSON: Wyświetla informacje o samochodzie w formacie JSON.
+
+        //Metoda ObliczSpalanie: Oblicza spalanie na podstawie przejechanych kilometrów i zużytego paliwa
+
+    }
+}
