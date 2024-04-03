d6d42e2fbebc38b1bb22c125abb8f053c2cd6803
The Fisher <114557710+RadomirSz@users.noreply.github.com>
2024-04-03 15:09:17 +0200

    Add files via upload

---
+++ Projektowanie Oprogramowania/Klasa 2/fasady/umlnaKod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umlNaKod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+    public abstract class Component 
+    {
+        public abstract void operation();
+    }
+    class Leaf : Component
+    {
+        public override void operation() { }
+    }
+    class Composite : Component 
+    {
+        public override void operation() { }
+        public void add() { }
+        public void remove() { }
+        public void getChild() { }
+
+        public List<Component> lista = new List<Component>();
+    }
+}
