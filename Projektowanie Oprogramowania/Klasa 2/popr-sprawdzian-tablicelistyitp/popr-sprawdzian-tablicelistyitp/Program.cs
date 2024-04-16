5424920d43ba5831ad093e7ebab67efb36bdd1d9
RadomirSz <r.szpruta@gmail.com>
2024-04-16 09:09:39 +0200

    .

---
+++ Projektowanie Oprogramowania/Klasa 2/popr-sprawdzian-tablicelistyitp/popr-sprawdzian-tablicelistyitp/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+int[] T = new int[30];
+Random r = new Random();
+for (int i = 0; i < 30; i++)
+{
+    T[i] = r.Next(100,1000);
+}
+int suma = T[0];
+int pierwsza = 0;
+int ostatnia = 0;
+int temppierwsza = 0;
+int tempostatnia = 0;
+
+int dlugosc = 1;
+int tempdlugosc = 1;
+for (int i = 1; i < 30; i++)
+{
+    if (T[i] > T[i - 1])
+    {
+        tempdlugosc++;
+        tempostatnia = T[i];
+        temppierwsza = T[i - tempdlugosc];
+    }
+    else 
+    {
+        pierwsza = temppierwsza;
+        ostatnia = tempostatnia;
+        dlugosc = tempdlugosc;
+    }
+}
+
+
+
+foreach (var item in T)
+{
+    Console.WriteLine(item);
+}
