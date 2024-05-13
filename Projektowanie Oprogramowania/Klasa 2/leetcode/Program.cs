15f11e00706c5c21307c175d1b3fd8f9edf882a1
RadomirSz <r.szpruta@gmail.com>
2024-05-13 23:40:32 +0200

    .

--- Projektowanie Oprogramowania/Klasa 2/leetcode/Program.cs
+++ Projektowanie Oprogramowania/Klasa 2/leetcode/Program.cs
@@ -26,7 +26,57 @@
 // }
 #endregion
 
+#region leet 2
 
+public string LongestPalindrome(string s) {
+    if(s.Length == 1) return s;
+    int Lenght = 0;
+    string Out = "";
+    for (int i = 0; i < s.Length-1; i++)
+    {
+        string tempStr = "";
+        for (int j = i; j < s.Length; j++)
+        {
+            tempStr += s[j];
+            if(isPalindrome(tempStr) && tempStr.Length > Lenght)
+            {
+                Out = tempStr;
+                Lenght = tempStr.Length;
+            }
+        }
+        if(Lenght == s.Length) return s;
+    }
+    return Out;
+}
+public bool isPalindrome(string s)
+{
+    if(s == reverse(s)) return true;
+    return false;
+}
+public string reverse(string s)
+{
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+
+/*
+pypon
+    def longestPalindrome(self, s):
+        if(len(s) == 1): return s
+        leng = 0
+        out = ""
+        for i in range(len(s)):
+            temp = ""
+            for j in range(i,len(s)):
+                temp += s[j]
+                if(temp == temp[::-1] and len(temp) > leng):
+                    leng = len(temp)
+                    out = temp
+            if(leng == len(s)): return s
+        return out
+*/
+#endregion
 
 
 
