f6d58587c116f8068812796333bbf0a4b60af1ac
The Fisher <114557710+RadomirSz@users.noreply.github.com>
2024-09-11 15:09:37 +0200

    Update lekcja2.py

--- Informatyka/lekcja2.py
+++ Informatyka/lekcja2.py
@@ -6,11 +6,16 @@ def czy_pierwsza(n):
 def czy_liczba_smitha(n):
     if(czy_pierwsza(n)): return False
 
-def suma_cyfr_liczby(n):
+def suma_cyfr_liczby(r):
     suma = 0
-    while(n>0):
-        suma += n%10
-        n = n//10
+    while(r>0):
+        suma += r%10
+        r = r//10
+    return suma
+def suma_listy(x):
+    suma = 0
+    for i in x:
+        suma += i
     return suma
 
 print(suma_cyfr_liczby(202))
@@ -34,8 +39,38 @@ while((n<=1) == False):
 print(f"suma: {suma}")
 
 if(czy_pierwsza(suma)):
-    print("suma jest liczbą parzystą")
+    print("suma jest liczbą pierwszą")
 else:
-    print("suma nie jest liczbą parzystą")
+    print("suma nie jest liczbą pierwszą")
 
 print(f"liczba różnych dzielników pierwszych: {liczba}")
+
+#zad 5
+
+
+t = int(input("daj liczbę: "))
+T = t
+suma2 = 0
+
+while((t<=1) == False):
+    for i in range(2,t+1):
+        if(t%i==0 and czy_pierwsza(i)):
+            t = t//i
+            suma2 += suma_cyfr_liczby(i)
+            break
+
+if(czy_pierwsza(T) == False):
+    sum = suma_cyfr_liczby(T)
+    print(sum)
+    print(suma2)
+    if(sum == suma2):
+        print("liczba ta jest liczbą smitha")
+
+
+
+
+
+
+
+
+
