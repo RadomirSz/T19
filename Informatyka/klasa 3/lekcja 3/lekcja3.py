1865dbceda930c039a519d4ca6490c62bb7a8ad1
The Fisher <114557710+RadomirSz@users.noreply.github.com>
2024-09-18 15:09:09 +0200

    Add files via upload

--- Informatyka/klasa 3/lekcja 3/lekcja3.py
+++ Informatyka/klasa 3/lekcja 3/lekcja3.py
@@ -6,15 +6,73 @@ def czy_pierwsza(n):
         if n%i==0: return False
     return True
 
+def dzielniki_wlasciwe(n):
+    ile = 0
+    for i in range(1,n):
+        if(n%i==0):
+            ile+=1
+    return ile
+
+def suma_czynników_pierwszych(n):
+    czynnik = 2
+    suma = 0
+    while n>1:
+        while n % czynnik == 0:
+            suma += czynnik
+            n //= czynnik
+        czynnik += 1
 
 plik = open("liczby1.txt","r")
 tab = list(map(int,plik.read().split()))
 
 ile = 0
-
 for liczba in tab:
     if czy_pierwsza(liczba): ile +=1
-print(ile)
+#print(ile)
 
 
 plik.close()
+
+
+plik2 = open("liczby2.txt","r")
+tab2 = list(map(int,plik2.read().split()))
+
+
+#z2
+#for i in tab2:
+#    if(i%2==0): print(i)
+
+#z3
+suma = 0
+for liczba in tab2:
+    suma += dzielniki_wlasciwe(liczba)
+#print(suma)
+
+
+#z4
+#cos zle
+# najwiekszaSuma = 0
+# liczba = 0
+# for i in tab2:
+#     if(suma_dzielnikow_pierwszych(i) > najwiekszaSuma):
+#         najwiekszaSuma = dzielniki_wlasciwe(i)
+#         liczba = i
+# print(liczba)
+
+#z5
+
+sumy_czynnikow = []
+for liczba in tab2:
+    sumy_czynnikow.append(suma_czynników_pierwszych(liczba))
+
+minimum = min(sumy_czynnikow)
+
+liczby = []
+for i in tab2:
+    if(suma_czynników_pierwszych(i) == minimum):
+        liczby.append(i)
+print(liczby)
+
+
+
+plik2.close()
