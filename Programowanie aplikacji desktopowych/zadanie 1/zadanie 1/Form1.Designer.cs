268ee1c2962fff929a04282b6f7276bdbab439e0
RadomirSz <r.szpruta@gmail.com>
2024-10-02 19:51:08 +0200

    prosze nie zapsuj sie

---
+++ Programowanie aplikacji desktopowych/zadanie 1/zadanie 1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace zadanie_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "bay";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(79, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(212, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(353, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(445, 144);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(77, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(355, 170);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(166, 27);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "NWD i NWW";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(415, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(349, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = " ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Programowanie aplikacji desktopowych/zadanie 1/zadanie 1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e){Close();}
+        private void textBox1_TextChanged(object sender, EventArgs e){}
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "bay") Close();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(DateTime.Now.ToString("D"));
+            MessageBox.Show(DateTime.Now.ToString("T"));
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e){}
+        private void textBox3_TextChanged(object sender, EventArgs e){}
+        private void button4_Click(object sender, EventArgs e)
+        {   
+            string Nwd = tools.nwd(int.Parse(textBox2.Text), int.Parse(textBox3.Text)).ToString();
+            string Nww = tools.nww(int.Parse(textBox2.Text), int.Parse(textBox3.Text)).ToString();
+            label2.Text = $"Nwd: {Nwd}, Nww: {Nww}";
+        }
+        private void label2_Click(object sender, EventArgs e){}
+    }
+    public static class tools
+    {
+        public static int nwd(int a, int b)
+        {
+            while (b > 0)
+            {
+                int r = a % b;
+                a = b;
+                b = r;
+            }
+            return a;
+        }
+        public static int nww(int a, int b) { return (a * b) / nwd(a, b); }
+    }
+}
