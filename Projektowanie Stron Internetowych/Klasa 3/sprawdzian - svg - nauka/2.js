fa1492062d9274e561f7c64dfb05eefbb2047ee3
RadomirSz <r.szpruta@gmail.com>
2024-10-03 22:58:12 +0200

    chyba sie nauczyłem

---
+++ Projektowanie Stron Internetowych/Klasa 3/sprawdzian - svg - nauka/2.js
@@ -0,0 +1,55 @@
+function funkcja() {
+    let shape = document.getElementById("Shape").value;
+    let size = document.getElementById("Size").value;
+    let color = document.getElementById("Color").value;
+
+    let svg = '';
+    if(document.getElementById("black").checked){
+        switch (shape) {
+            case "rect":
+                svg = `<rect x="50vw" y="10vh" width="${size}" height="${size/2}" fill="${color}" stroke="black"></rect>`
+                break;
+            case "circle":
+                svg = `<circle cx="50vw" cy="10vh" r="${size/2}" fill="${color}" stroke="black"></circle>`
+                break;
+            case "ellipse":
+                svg = `<ellipse cx="50vw" cy="10vh" rx="${size}" ry="${size/2}" fill="${color}" stroke="black"></ellipse>`
+                break;
+        
+            default:
+                break;
+        }
+    }else if(document.getElementById("blue").checked){
+        switch (shape) {
+            case "rect":
+                svg = `<rect x="50vw" y="10vh" width="${size}" height="${size/2}" fill="${color}" stroke="blue"></rect>`
+                break;
+            case "circle":
+                svg = `<circle cx="50vw" cy="10vh" r="${size/2}" fill="${color}" stroke="blue"></circle>`
+                break;
+            case "ellipse":
+                svg = `<ellipse cx="50vw" cy="10vh" rx="${size}" ry="${size/2}" fill="${color}" stroke="blue"></ellipse>`
+                break;
+        
+            default:
+                break;
+        }
+    }
+    else{
+        switch (shape) {
+            case "rect":
+                svg = `<rect x="50vw" y="10vh" width="${size}" height="${size/2}" fill="${color}"></rect>`
+                break;
+            case "circle":
+                svg = `<circle cx="50vw" cy="10vh" r="${size/2}" fill="${color}"></circle>`
+                break;
+            case "ellipse":
+                svg = `<ellipse cx="50vw" cy="10vh" rx="${size}" ry="${size/2}" fill="${color}" ></ellipse>`
+                break;
+        
+            default:
+                break;
+        }
+    }
+    document.getElementById("kontener").innerHTML = svg;
+}
