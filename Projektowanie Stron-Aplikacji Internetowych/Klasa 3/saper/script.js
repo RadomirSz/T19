d34d9af5d8c077c1e78d7925cbd299b01bcba35f
RadomirSz <r.szpruta@gmail.com>
2024-10-15 23:19:36 +0200

    minesweeper update

--- Projektowanie Stron-Aplikacji Internetowych/Klasa 3/saper/script.js
+++ Projektowanie Stron-Aplikacji Internetowych/Klasa 3/saper/script.js
@@ -2,12 +2,8 @@
 var emotki = ["😊","🙃","☹️","😎","💣"]; // default - click - lose - win - bomb
 
 function start() {
-    $('#startButton').attr
+    change();
 }
-function changeSize() {
-    fieldSize = parseInt($("#fieldSize").val());
-    
-  }
 
 function change() {
     //$('#fieldSize').on('change', function() {change();});
@@ -15,58 +11,69 @@ function change() {
 
     switch (size) {
         case 'small':
+            $('#frame').css('width',"15vw");
             createField(8,10);
             break;
         case 'medium':
+            $('#frame').css('width',"22vw");
             createField(12,22);
             break;
         case 'big':
+            $('#frame').css('width',"28vw");
             createField(16,40);
             break;
         default:
             alert('Wybierz rozmiar planszy!');
             break;
     }
-    start();
 }
-
+var board = $(".field");
 function createField(fieldSize, mineCount) 
 {
-  var board = $(".field");
-  var arrays = new Array();
+    board = $(".field");
+    //var arrays = new Array();
+    board.empty();
+    for (var i = 0; i < fieldSize; i++) 
+    {
+        var row = $('<div>').addClass('row');
+        for (var j = 0; j < fieldSize; j++) 
+        {
+            var col = $('<div>').addClass('col hidden');
+            let number = (i+1) + "-" + (j+1);
+            col.addClass(number);
+            col.data('mine', false);
+            col.data('flagged', false);
 
-for (var i = 0; i < fieldSize; i++) {
-    var row = $('<div>').addClass('row');
-    var newarray = new Array();
-    for (var j = 0; j < fieldSize; j++) {
-        var col = $('<div>').addClass('col').addId(i +"-"+ j);
-        
-        
-        cell.data('revealed', false);
-        cell.data('flagged', false);
-        cell.text("");
-        cell.click(revealCell);
-        cell.contextmenu(toggleFlag);
-        
-        newarray.append(false);
-        row.append(col);
-    }
-    board.append(row);
-    arrays.append(newarray);
-}
-
-for (var k = 0; k < mineCount; k++) {
-    let flag = true;
-        do {
-        var randomRow = Math.floor(Math.random() * fieldSize);
-        var randomCol = Math.floor(Math.random() * fieldSize);
-        let cell = "." + randomRow + "-" + randomCol;
-        var randomCell = $(cell);
-        if (!randomCell.data('mine')) { 
-            randomCell.data('mine', true);
-            flag = false;
+            //col.contextmenu(toggleFlag);
+            
+            row.append(col);
         }
-    } while (flag);
-}
+        board.append(row);
+    }
+
+    for (var k = 0; k < mineCount; k++) {
+        let flag = true;
+        do 
+        {
+            var randomRow = Math.floor(Math.random() * fieldSize);
+            var randomCol = Math.floor(Math.random() * fieldSize);
+            let cell = "." + randomRow + "-" + randomCol;
+            var randomCell = $(cell);
+            if (!randomCell.data('mine')) 
+            { 
+                randomCell.data('mine', true);
+                console.log(cell + " got a mine");
+                flag = false;
+            }
+        } while (flag);
+    }
 
 }
+
+board.on('click', '.col.hidden', function (){
+    console.log("klik");
+    if($(this).data('mine'))
+    {
+        console.log("klik z miną");
+    }
+})
