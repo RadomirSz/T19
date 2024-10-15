55b78492e2bdb50916c7e61656f63be0e219492b
The Fisher <114557710+RadomirSz@users.noreply.github.com>
2024-10-15 11:24:19 +0200

    Update zadanie7.py

--- Informatyka/klasa 3/lekcja6/zadanie7.py
+++ Informatyka/klasa 3/lekcja6/zadanie7.py
@@ -6,12 +6,12 @@ def first_even_number(T):
     prawy = len(T)
     while lewy<prawy:
         srodek = (lewy + prawy)//2
-        if(T[srodek] % 2 == 0):
+        if T[srodek] % 2 == 0:
             prawy = srodek
         else:
             lewy = srodek+1
-        if T[srodek] %2==0 and T[srodek-1]%2==1 and srodek > 2:
-            return T[srodek]
+        # if T[srodek] %2==0 and T[srodek-1]%2==1 and srodek > 2:
+        #     return T[srodek]
     return T[lewy]
 
 
