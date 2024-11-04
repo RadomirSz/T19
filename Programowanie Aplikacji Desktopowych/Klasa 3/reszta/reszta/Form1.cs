56e038111db368f721355f6f7e85a869ac91bc40
RadomirSz <r.szpruta@gmail.com>
2024-11-04 20:41:42 +0100

    a

--- Programowanie Aplikacji Desktopowych/Klasa 3/reszta/reszta/Form1.cs
+++ Programowanie Aplikacji Desktopowych/Klasa 3/reszta/reszta/Form1.cs
@@ -12,9 +12,17 @@ namespace reszta
 {
     public partial class Form1 : Form
     {
+        public FlowLayoutPanel flowLayoutPanel;
+
         public Form1()
         {
             InitializeComponent();
+            flowLayoutPanel = new FlowLayoutPanel
+            {
+                Size = new Size(1000, 500),
+                Location = new Point(10, 150)
+            };
+            Controls.Add(flowLayoutPanel);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -24,16 +32,27 @@ namespace reszta
 
         private void button1_Click(object sender, EventArgs e)
         {
+            flowLayoutPanel.Controls.Clear();
             int kwota = int.Parse(textBox1.Text);
 
-            int[] nominaly = new int[6]{1,2,5,10,20,50};
+            string[] nominalyZdjecia = {"50.jpg", "20.jpg", "10.jpg", "5.jpg", "2.jpg", "1.jpg"};
+            int[] nominaly = new int[6]{50,20,10,5,2,1};
             List<int> reszta = new List<int>();
             while (kwota > 0)
             {
-                for (int i = 5; i > 0; i--)
+                for (int i = 5; i >= 0; i--)
                 {
                     if (kwota >= nominaly[i])
                     {
+                        Image image = Image.FromFile(nominalyZdjecia[i]);
+                        
+                        PictureBox pictureBox = new PictureBox();
+
+                        pictureBox.Image = image;
+                        pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+
+                        flowLayoutPanel.Controls.Add(pictureBox);
+
                         reszta.Add(nominaly[i]);
                         kwota -= nominaly[i];
                         break;
@@ -47,6 +66,8 @@ namespace reszta
                 output += item + " ";
             }
             label1.Text = output;
+
+
         }
 
         private void label1_Click(object sender, EventArgs e)
