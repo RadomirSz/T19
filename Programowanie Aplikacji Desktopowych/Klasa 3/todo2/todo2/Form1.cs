5f21f1d7253396599700e7c6400d6155dec53f9d
radomirsz <r.szpruta@gmail.com>
2024-11-13 13:20:13 +0100

    a

---
+++ Programowanie Aplikacji Desktopowych/Klasa 3/todo2/todo2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace todo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public string path = "..\\..\\data.txt";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string sex;
+            if (radioButton1.Checked) sex = "Male";
+            else if (radioButton2.Checked) sex = "Female";
+            else sex = "Null";
+            string description = richTextBox1.Text;
+
+            StreamWriter writer = new StreamWriter(path);
+            writer.WriteLine(name);
+            writer.WriteLine(sex);
+            writer.WriteLine(description);
+            writer.WriteLine();
+            writer.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader(path);
+            string person = "";
+            while (!reader.EndOfStream)
+            {
+                person += reader.ReadLine();
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
