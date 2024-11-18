61996bf57cc19b90e9556f219de45534be4ecba1
RadomirSz <r.szpruta@gmail.com>
2024-11-18 17:29:21 +0100

    something changed

--- Programowanie Obiektowe/Klasa 3/interface-ksiegarnia/interface-ksiegarnia/Program.cs
+++ Programowanie Obiektowe/Klasa 3/interface-ksiegarnia/interface-ksiegarnia/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Interfejsy_3
 {
-    class Book : IComparable<Book>, IEquatable<Book>
+    class Book
     {
         public string title;
         public string author;
@@ -100,3 +100,46 @@ namespace Interfejsy_3
         }
     }
 }
+
+
+
+/*
+Interfejsy
+ Zdefiniuj klasę Book, która ma reprezentować informacje o książce, takie jak tytuł,
+autor, rok wydania i cena.
+
+
+ Zaimplementuj interfejs IComparable<Book> w klasie Book, który pozwala na
+porównywanie obiektów typu Book według ich ceny. Aby to zrobić, zdefiniuj metodę
+CompareTo(Book other), która zwraca liczbę całkowitą oznaczającą relację pomiędzy
+bieżącym obiektem a innym obiektem typu Book.
+
+
+ Zdefiniuj konstruktor klasy Book, który przyjmuje jako parametry tytuł, autora, rok
+wydania i cenę książki i inicjalizuje odpowiednie pola klasy.
+
+
+ Zdefiniuj metodę ToString() klasy Book, która zwraca reprezentację tekstową obiektu
+typu Book, składającą się z tytułu, autora, roku wydania i ceny książki, oddzielonych
+przecinkami.
+
+
+ Zdefiniuj klasę Program, która zawiera metodę Main, w której utwórz listę książek
+typu Book i dodaj do niej kilka przykładowych obiektów.
+
+
+ Posortuj listę książek według ceny, używając metody Sort klasy List<T>. Wyświetl
+posortowaną listę na konsoli, używając metody ToString() klasy Book.
+
+
+ Posortuj listę książek według innych kryteriów, np. daty publikacji, autora, tytułu,
+używając metod OrderBy, OrderByDescending i ThenBy z przestrzeni nazw
+System.Linq. Wyświetl posortowane listy na konsoli, używając metody ToString() klasy
+Book.
+
+
+Sortowanie według ceny:
+Sortowanie według daty publikacji:
+Sortowanie według autora:
+Sortowanie według ceny nierosnąco a następnie według roku od najstarszej książki:	
+*/
