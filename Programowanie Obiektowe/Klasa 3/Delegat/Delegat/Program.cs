c57b57d54fee9cb934de01d48db94dd20f74f7b1
The Fisher <114557710+RadomirSz@users.noreply.github.com>
2024-12-02 15:08:34 +0100

    delegaty

---
+++ Programowanie Obiektowe/Klasa 3/Delegat/Delegat/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegat
+{
+    internal class Program
+    {
+
+        public delegate void NotificationHandler(string message);
+
+        public class EmailNotifier
+        {
+            public void SendEmail(string message)
+            {
+                Console.WriteLine($"email wyslany: {message}");
+            }
+        }
+
+        public class SMSNotifier
+        {
+            public void SendSMS(string message)
+            {
+                Console.WriteLine($"sms wyslany: {message}");
+            }
+        }
+
+        public class PushNotifier
+        {
+            public void SendPushNotifier(string message)
+            {
+                Console.WriteLine($"powiadomienie wysłane: {message}");
+            }
+        }
+
+        public class NotificationManager
+        {
+            public NotificationHandler Notify;
+
+            public void AddNotificationMethod(NotificationHandler handler)
+            {
+                Notify += handler;
+            }
+
+            public void RemoveNotificationMethod(NotificationHandler handler)
+            {
+                Notify -= handler;
+            }
+
+            public void SendNotification(string message)
+            {
+                Notify?.Invoke(message);
+
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            var emailNotifier = new EmailNotifier();
+            //emailNotifier.SendEmail("siema");
+            var smsNotifier = new SMSNotifier();
+            //smsNotifier.SendSMS("siema");
+            var pushNotifier = new PushNotifier();
+            //pushNotifier.SendPushNotifier("siema");
+
+            var notificationMenager = new NotificationManager();
+
+            notificationMenager.AddNotificationMethod(emailNotifier.SendEmail);
+            notificationMenager.AddNotificationMethod(smsNotifier.SendSMS);
+            notificationMenager.AddNotificationMethod(pushNotifier.SendPushNotifier);
+
+            notificationMenager.SendNotification("1 wiadomość");
+            
+            Console.WriteLine();
+
+
+            notificationMenager.RemoveNotificationMethod(smsNotifier.SendSMS);
+
+            notificationMenager.SendNotification("2 wiadomość");
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Programowanie Obiektowe/Klasa 3/Delegat/Delegat/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Delegat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Delegat")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("78c7c3cf-551e-4ac8-bd09-6519569bcd55")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
