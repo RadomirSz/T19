eb214e32c43037e4af941d7d6c64c13f022a364b
RadomirSz <114557710+RadomirSz@users.noreply.github.com>
2025-03-05 13:21:18 +0100

    Add files via upload

---
+++ Programowanie Aplikacji Desktopowych/Klasa 3/test/test/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+
+            Regex r = new Regex(@"^\d{7,9}$");
+            
+            if (!int.TryParse(text, out int number) || !r.IsMatch(text)) 
+            {
+                MessageBox.Show("Zły input");
+                return; 
+            }
+            
+            
+
+            label1.Text = czysiedzieliprzez7(number).ToString();
+        }
+        public bool czysiedzieliprzez7(int number)
+        {
+            string sr = "";
+            while (number > 0)
+            {
+                int kon = number % 100;
+                sr = sr + kon % 7;
+                number = number / 100;
+            }
+            int sum = 0;
+            foreach (var item in sr)
+            {
+                sum += item;
+            }
+            if(sum %7 == 0) return true;
+            return false;
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
---
+++ Programowanie Aplikacji Desktopowych/Klasa 3/test/test/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("8d0a3a9f-d2a2-4074-8e04-72aeda821e99")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
