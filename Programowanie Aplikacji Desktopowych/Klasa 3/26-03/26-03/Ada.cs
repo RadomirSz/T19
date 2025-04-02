2af98f8518fd359d3dfe26ae71ebdf3389817113
RadomirSz <114557710+RadomirSz@users.noreply.github.com>
2025-04-02 13:41:18 +0300

    Add files via upload

---
+++ Programowanie Aplikacji Desktopowych/Klasa 3/26-03/26-03/Ada.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_03
+{
+    public class Ada : IAda, IDao
+    {
+        public const int limit = 10;
+        public int dlugosc(string t)
+        {
+            if (t.Length > limit) throw new ArgumentException();
+            return limit - t.Length;
+        }
+    }
+}
