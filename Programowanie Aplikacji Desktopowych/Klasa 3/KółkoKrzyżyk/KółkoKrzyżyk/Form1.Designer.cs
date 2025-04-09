a6c1f0cd41b45929d60e89b4a901e00a7e2c7d41
RadomirSz <114557710+RadomirSz@users.noreply.github.com>
2025-04-09 14:19:42 +0300

    Add files via upload

---
+++ Programowanie Aplikacji Desktopowych/Klasa 3/KółkoKrzyżyk/KółkoKrzyżyk/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace KółkoKrzyżyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(35, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(38, 37);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(45, 201);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "losuj";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(82, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(38, 37);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(126, 12);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(38, 37);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(35, 55);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(38, 37);
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(82, 55);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(38, 37);
+            pictureBox5.TabIndex = 5;
+            pictureBox5.TabStop = false;
+            pictureBox5.Click += pictureBox5_Click;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(126, 55);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(38, 37);
+            pictureBox6.TabIndex = 6;
+            pictureBox6.TabStop = false;
+            pictureBox6.Click += pictureBox6_Click;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(35, 98);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(38, 37);
+            pictureBox7.TabIndex = 7;
+            pictureBox7.TabStop = false;
+            pictureBox7.Click += pictureBox7_Click;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Location = new Point(82, 98);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(38, 37);
+            pictureBox8.TabIndex = 8;
+            pictureBox8.TabStop = false;
+            pictureBox8.Click += pictureBox8_Click;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Location = new Point(126, 98);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(38, 37);
+            pictureBox9.TabIndex = 9;
+            pictureBox9.TabStop = false;
+            pictureBox9.Click += pictureBox9_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+    }
+}
---
+++ Programowanie Aplikacji Desktopowych/Klasa 3/KółkoKrzyżyk/KółkoKrzyżyk/Form1.cs
@@ -0,0 +1,134 @@
+namespace KółkoKrzyżyk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] losy = new int[9];
+            Random random = new Random();
+            string oute = string.Empty;
+            losy[0] = random.Next(0, 9);
+            for (int i = 1; i < 8; i++)
+            {
+                int x = random.Next(0, 9);
+                while (losy.Contains(x))
+                {
+                    x = random.Next(0, 9);
+                }
+                losy[i] = x;
+                //MessageBox.Show(losy[i].ToString());
+            }
+            int[] kolkaikrzyzyki = new int[9];
+            for (int i = 0; i < 9; i++)
+            {
+                if (i < 5) kolkaikrzyzyki[losy[i]] = 1;
+                else kolkaikrzyzyki[losy[i]] = 0;
+            }
+            foreach (var item in kolkaikrzyzyki)
+            {
+                oute += item;
+                //MessageBox.Show(item.ToString());
+            }
+            //MessageBox.Show(oute);
+            for (int i = 0; i < 9; i++)
+            {
+                setPictureBox(i, kolkaikrzyzyki[i]);
+            }
+        }
+        public void setPictureBox(int box, int which)
+        {
+            switch (box)
+            {
+                case 0:
+                    if (which == 1) pictureBox1.Image = Image.FromFile("liga.png");
+                    else pictureBox1.Image = Image.FromFile("prysznic.png");
+                    break;
+                case 1:
+                    if (which == 1) pictureBox2.Image = Image.FromFile("liga.png");
+                    else pictureBox2.Image = Image.FromFile("prysznic.png");
+                    break;
+                case 2:
+                    if (which == 1) pictureBox3.Image = Image.FromFile("liga.png");
+                    else pictureBox3.Image = Image.FromFile("prysznic.png");
+                    break;
+                case 3:
+                    if (which == 1) pictureBox4.Image = Image.FromFile("liga.png");
+                    else pictureBox4.Image = Image.FromFile("prysznic.png");
+                    break;
+                case 4:
+                    if (which == 1) pictureBox5.Image = Image.FromFile("liga.png");
+                    else pictureBox5.Image = Image.FromFile("prysznic.png");
+                    break;
+                case 5:
+                    if (which == 1) pictureBox6.Image = Image.FromFile("liga.png");
+                    else pictureBox6.Image = Image.FromFile("prysznic.png");
+                    break;
+                case 6:
+                    if (which == 1) pictureBox7.Image = Image.FromFile("liga.png");
+                    else pictureBox7.Image = Image.FromFile("prysznic.png");
+                    break;
+                case 7:
+                    if (which == 1)pictureBox8.Image = Image.FromFile("liga.png");
+                    else pictureBox8.Image = Image.FromFile("prysznic.png");
+                    break;
+                case 8:
+                    if (which == 1) pictureBox9.Image = Image.FromFile("liga.png");
+                    else pictureBox9.Image = Image.FromFile("prysznic.png");
+                    break;
+                default:
+                    MessageBox.Show("how did we get here?");
+                    break;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
