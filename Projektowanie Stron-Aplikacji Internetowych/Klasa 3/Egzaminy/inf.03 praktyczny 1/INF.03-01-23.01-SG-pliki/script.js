be33bf7a0868f8d748be20d05fc8d9535f8a8ced
RadomirSz <r.szpruta@gmail.com>
2025-04-25 00:40:22 +0200

    porządek w folderach

---
+++ Projektowanie Stron-Aplikacji Internetowych/Klasa 3/Egzaminy/inf.03 praktyczny 1/INF.03-01-23.01-SG-pliki/script.js
@@ -0,0 +1,3 @@
+function func(){
+    document.getElementById("output").innerHTML = "Liczba potrzebnych puszek: " + Math.ceil(document.getElementById("number").value/4);
+}   
--- Projektowanie Stron-Aplikacji Internetowych/Klasa 3/inf.03 praktyczny 1/INF.03-01-23.01-SG-pliki/script.js
+++
@@ -1,3 +0,0 @@
-function func(){
-    document.getElementById("output").innerHTML = "Liczba potrzebnych puszek: " + Math.ceil(document.getElementById("number").value/4);
-}   
