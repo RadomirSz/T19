28a30620b3b8bca50457529f9c36f9a705b8bbe0
RadomirSz <r.szpruta@gmail.com>
2025-05-02 15:00:44 +0200

    lekcja18 kontynuacja

--- Informatyka/klasa 3/lekcja18/sql.sql
+++ Informatyka/klasa 3/lekcja18/sql.sql
@@ -1,8 +1,7 @@
 -- 12.1
 SELECT * FROM klienci ORDER BY nazwisko;
 
--- 12.2
-
+-- 12_2
 SELECT DISTINCT marka, model 
 FROM samochody 
 WHERE rocznik >= 2010 
@@ -10,4 +9,8 @@ AND rocznik <= 2020
 AND moc > 100 
 ORDER BY marka, model;
 
-
+-- 12_3
+SELECT klienci.Id, klienci.imie, klienci.nazwisko, Count(samochody.id) AS PoliczOfid
+FROM klienci LEFT JOIN samochody ON klienci.Id = samochody.wlasciciel
+GROUP BY klienci.Id, klienci.imie, klienci.nazwisko
+ORDER BY Count(samochody.id) DESC;
