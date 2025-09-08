96a0afa62df831b97b197c41028828b6831af896
RadomirSz <r.szpruta@gmail.com>
2025-09-08 13:35:25 +0300

    a

--- Programowanie Aplikacji Mobilnych/klasa 4/test1/app/src/main/java/com/example/test1/MainActivity.java
+++ Programowanie Aplikacji Mobilnych/klasa 4/test1/app/src/main/java/com/example/test1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.test1;
 
 import android.os.Bundle;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,7 +26,10 @@ public class MainActivity extends AppCompatActivity {
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+                TextView tv = findViewById(R.id.wynik);
+                if(progress == 0) tv.setText("malo");
+                if(progress == 1) tv.setText("srednio");
+                if(progress == 2) tv.setText("duzio");
             }
 
             @Override
