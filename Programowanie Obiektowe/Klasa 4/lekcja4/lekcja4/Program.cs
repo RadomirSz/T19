1b192e61e9c087e97ea71ea37d37a4768816d252
RadomirSz <r.szpruta@gmail.com>
2025-10-09 12:51:39 +0200

    lekcja4 obiektowka

---
+++ Programowanie Obiektowe/Klasa 4/lekcja4/lekcja4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lekcja4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Przykład użycia ConfigurationManager
+            var config = ConfigurationManager.Instance;
+            config.SetSetting("AppName", "My Application");
+            Console.ReadKey();
+        }
+    }
+
+    public class ConfigurationManager
+    {
+        private static ConfigurationManager _instance;
+        private Dictionary<string, object> _settings;
+        
+        private ConfigurationManager() 
+        {
+            _settings = new Dictionary<string, object>();
+        }
+        
+        public static ConfigurationManager Instance 
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ConfigurationManager();
+                }
+                return _instance;
+            }
+        }
+        
+        public void SetSetting(string key, object value)
+        {
+            _settings[key] = value;
+        }
+        
+        public T GetSetting<T>(string key, T defaultValue = default)
+        {
+            if (_settings.TryGetValue(key, out var value) && value is T typedValue)
+            {
+                return typedValue;
+            }
+            return defaultValue;
+        }
+    }
+}
