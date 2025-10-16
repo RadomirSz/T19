cef9befa7e86988b1ac1487c8a2cd0ec1cc85d1b
RadomirSz <r.szpruta@gmail.com>
2025-10-16 12:21:42 +0200

    16 10 desktopowka

---
+++ Programowanie Aplikacji Desktopowych/klasa 4/16_10/16_10/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace _16_10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Koszyk koszyk;
+        public int currentProduct {  get; set; }
+        public MainWindow()
+        {
+            koszyk = new Koszyk();
+            InitializeComponent();
+            InitializeProductViewer();
+            currentProduct = 0;
+        }
+        
+
+        public void InitializeProductViewer()
+        {
+            dane.Text = koszyk.Produkty.ElementAt(0).Nazwa;
+            cena.Text = koszyk.Produkty.ElementAt(0).Cena.ToString();
+
+            if(currentProduct == 0 ) btnPrev.IsEnabled = false;
+            if (currentProduct == koszyk.Produkty.Count - 1) btnNext.IsEnabled = false;
+        }
+
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            btnNext.IsEnabled = true;
+            if (currentProduct == 0) btnPrev.IsEnabled = false;
+
+        }
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            btnPrev.IsEnabled = true;
+            if (currentProduct == koszyk.Produkty.Count - 1) btnNext.IsEnabled = false;
+        }
+    }
+}
---
+++ Programowanie Aplikacji Desktopowych/klasa 4/16_10/16_10/Zniżki.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _16_10
+{
+    internal class ZniżkaKwotowa : IZniżkaStrategia
+    {
+        public int obliczZniżkę(int k, int x)
+        {
+            return k - x;
+        }
+    }
+
+    internal class ZniżkaProcentowa : IZniżkaStrategia
+    {
+        public int obliczZniżkę(int k, int x)
+        {
+            return (k * (100 - x)) / 100;
+        }
+    }
+}
