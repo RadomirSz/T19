d46dab53ab1bc486a7b7034bdaf37e167750451c
RadomirSz <r.szpruta@gmail.com>
2025-10-28 09:12:15 +0100

    28_10 desktop sqlite

---
+++ Programowanie Aplikacji Desktopowych/klasa 4/28_10/28_10/OsobaBiznes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_10
+{
+    class OsobaBiznes
+    {
+        OsobaContext Context;
+        public OsobaBiznes()
+        {
+            Context = new OsobaContext();
+        }
+
+        public List<Osoba> pobierzOsoby()
+        {
+            return Context.Osoby.ToList();
+        }
+    }
+}
---
+++ Programowanie Aplikacji Desktopowych/klasa 4/28_10/28_10/OsobaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _28_10
+{
+    class OsobaContext : DbContext
+    {
+        public DbSet<Osoba> Osoby { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=kurczak.db;");
+        }
+    }
+}
