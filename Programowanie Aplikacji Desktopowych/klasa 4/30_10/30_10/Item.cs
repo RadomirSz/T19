c7f35bf4e82388f8f3a56196dd105963cb09a221
RadomirSz <r.szpruta@gmail.com>
2025-10-30 12:20:57 +0100

    30-10

---
+++ Programowanie Aplikacji Desktopowych/klasa 4/30_10/30_10/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_10
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
---
+++ Programowanie Aplikacji Desktopowych/klasa 4/30_10/30_10/bazaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _30_10
+{
+    public class bazaContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=../../../baza.db; Mode=ReadOnly");
+        }
+    }
+}
