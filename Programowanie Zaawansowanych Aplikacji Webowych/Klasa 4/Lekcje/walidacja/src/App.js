c4cfef16a1374d1821b068360d3786e649e5c91f
RadomirSz <r.szpruta@gmail.com>
2025-10-30 13:01:59 +0100

    webowka - lekcja walidacja

--- Programowanie Zaawansowanych Aplikacji Webowych/Klasa 4/Lekcje/walidacja/src/App.js
+++ Programowanie Zaawansowanych Aplikacji Webowych/Klasa 4/Lekcje/walidacja/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Props from './WalidacjaDuzy.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Props propArray={[2,4,6]} propBool={false}/>
     </div>
   );
 }
