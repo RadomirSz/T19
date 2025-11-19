5f05047bf7240918c4d2babd36fbc8787cd145d9
RadomirSz <r.szpruta@gmail.com>
2025-11-19 15:58:40 +0100

    19_11 mobilka

---
+++ Programowanie Aplikacji Mobilnych/klasa 4/19_11/app/src/main/java/com/example/a19_11/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a19_11;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioButton rad1,rad2,rad3;
+    CheckBox check1, check2;
+    Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
