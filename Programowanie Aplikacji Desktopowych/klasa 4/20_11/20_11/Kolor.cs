1a43de971645e804cac4d841921d23bf1f8461e3
RadomirSz <r.szpruta@gmail.com>
2025-11-20 12:20:16 +0100

    20 11 desktopówka

---
+++ Programowanie Aplikacji Desktopowych/klasa 4/20_11/20_11/Kolor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_11
+{
+    class Kolor
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+    }
+}
