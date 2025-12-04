d9022f946d15c7c13f8a620e8a265c48348171d6
RadomirSz <r.szpruta@gmail.com>
2025-12-04 12:56:03 +0100

    mvvm NotifyPropertyChanged

---
+++ Programowanie Aplikacji Desktopowych/klasa 4/4_12/4_12/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace _4_12
+{
+    public partial class ViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(Fullname))]
+        private string firstname = "";
+            
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(Fullname))]
+        private string lastname ="";
+
+        public string Fullname => $"{Firstname} {Lastname}";
+    }
+}
