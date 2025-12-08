c0e69eb7b5a7d2aa55924a4ecc969834d5a6f729
RadomirSz <r.szpruta@gmail.com>
2025-12-09 00:57:42 +0100

    bam

--- Programowanie Aplikacji Mobilnych/klasa 4/Java + Xml/8_12/app/src/main/java/com/example/a8_12/MainActivity.java
+++ Programowanie Aplikacji Mobilnych/klasa 4/Java + Xml/8_12/app/src/main/java/com/example/a8_12/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.a8_12;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +15,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvGlosnosc;
+    SeekBar seekBar;
+    ToggleButton tgbtn;
+    ListView lvHistoria;
+    Button btnZas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +33,49 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        tvGlosnosc = findViewById(R.id.tvGlosnosc);
+        seekBar = findViewById(R.id.seekBar);
+        tgbtn = findViewById(R.id.tgButton);
+        lvHistoria = findViewById(R.id.LvHistoria);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tvGlosnosc.setText("Głośność: " + i + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        tgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tgbtn.isChecked())
+                    tgbtn.setBackgroundColor(getResources().getColor(R.color.toggleoff));
+                else
+                    tgbtn.setBackgroundColor(getResources().getColor(R.color.toggleon));
+            }
+        });
+
+
+        ArrayList<String> historyList = new ArrayList<>();
+        ArrayAdapter<String> historyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, historyList);
+        lvHistoria.setAdapter(historyAdapter);
+
+
+        btnZas = findViewById(R.id.btnZas);
+        btnZas.setOnClickListener(v -> {
+            String entry = "Głośność: " + seekBar.getProgress() + "%";
+            historyList.add(entry);
+            historyAdapter.notifyDataSetChanged();
         });
     }
 }
