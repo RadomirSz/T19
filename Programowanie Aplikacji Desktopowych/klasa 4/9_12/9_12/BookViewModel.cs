7d5bd80b15e6f1643019ef816e4117017dbc1eeb
RadomirSz <r.szpruta@gmail.com>
2025-12-09 08:09:46 +0100

    9 12 desktopowka

---
+++ Programowanie Aplikacji Desktopowych/klasa 4/9_12/9_12/BookViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace _9_12
+{
+    public partial class BookViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string tytul;
+
+        [ObservableProperty]
+        private string autor;
+    }
+}
---
+++ Programowanie Aplikacji Desktopowych/klasa 4/9_12/9_12/LibraryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_12
+{
+    public partial class LibraryViewModel : ObservableObject
+    {
+
+        public ObservableCollection<BookViewModel> Books { get; } = new();
+
+        public LibraryViewModel()
+        {
+            Books.Add(new BookViewModel { Tytul = "Dziady", Autor = "Adam Mickiewicz" });
+            Books.Add(new BookViewModel { Tytul = "Pan Tadeusz", Autor = "Adam Mickiewicz" });
+            Books.Add(new BookViewModel { Tytul = "Pan Mickiewicz", Autor = "Adam Tadeusz" });
+        }
+    }
+}
