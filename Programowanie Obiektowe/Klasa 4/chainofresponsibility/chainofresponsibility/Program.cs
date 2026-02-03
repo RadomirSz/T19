8a1ad91bcf51e98835245a233ae7e00c42d16fde
RadomirSz <r.szpruta@gmail.com>
2026-02-03 09:04:52 +0100

    wzorce pob

---
+++ Programowanie Obiektowe/Klasa 4/chainofresponsibility/chainofresponsibility/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace chainofresponsibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TicketResolver resolver = new TicketResolver();
+
+            Ticket ticket1 = new Ticket("Password Reset", 1);
+            Ticket ticket2 = new Ticket("Software Installation", 2);
+            Ticket ticket3 = new Ticket("Network Issue", 3);
+            resolver.Handler.HandleRequest(ticket1);
+            resolver.Handler.HandleRequest(ticket2);
+            resolver.Handler.HandleRequest(ticket3);
+        }
+    }
+
+
+    public class Ticket(string name, int priority)
+    {
+        public string Name { get; } = name;
+        public int Priority { get; } = priority;
+    }
+
+    public class TicketResolver
+    {
+        public SupportHandler Handler;
+        public TicketResolver()
+        {
+            var levelOne = new LevelOneSupport();
+            var levelTwo = new LevelTwoSupport();
+            var levelThree = new LevelThreeSupport();
+            levelOne.SetNext(levelTwo).SetNext(levelThree);
+            Handler = levelOne;
+        }
+    }
+    public abstract class SupportHandler
+    {
+        protected SupportHandler? NextHandler { get; private set; }
+        public SupportHandler SetNext(SupportHandler handler)
+        {
+            NextHandler = handler;
+            return handler;
+        }
+        public abstract void HandleRequest(Ticket ticket);
+    }
+    public class LevelOneSupport : SupportHandler
+    {
+        public override void HandleRequest(Ticket ticket)
+        {
+            if (ticket.Priority <= 1)
+            {
+                Console.WriteLine($"Level One Support resolved ticket: {ticket.Name}");
+            }
+            else
+            {
+                NextHandler?.HandleRequest(ticket);
+            }
+        }
+    }
+    public class LevelTwoSupport : SupportHandler
+    {
+        public override void HandleRequest(Ticket ticket)
+        {
+            if (ticket.Priority <= 2)
+            {
+                Console.WriteLine($"Level Two Support resolved ticket: {ticket.Name}");
+            }
+            else
+            {
+                NextHandler?.HandleRequest(ticket);
+            }
+        }
+    }
+    public class LevelThreeSupport : SupportHandler
+    {
+        public override void HandleRequest(Ticket ticket)
+        {
+            if (ticket.Priority <= 3)
+            {
+                Console.WriteLine($"Level Three Support resolved ticket: {ticket.Name}");
+            }
+            else
+            {
+                NextHandler?.HandleRequest(ticket);
+            }
+        }
+    }
+}
---
+++ Programowanie Obiektowe/Klasa 4/state/state/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace state;
+
+public interface IDoorState
+{
+ void PressButton(Door door);
+ string Name { get; }
+}
+
+public sealed class Door
+{
+    private IDoorState _state;
+    public Door(IDoorState initialState) => _state = initialState;
+    public string StateName => _state.Name;
+    public void SetState(IDoorState newState) => _state = newState;
+    public void PressButton() => _state.PressButton(this);
+}
+
+public sealed class OpenState : IDoorState
+{
+    public string Name => "Open";
+    public void PressButton(Door door) => door.SetState(new ClosedState());
+}
+
+public sealed class ClosedState : IDoorState
+{
+    public string Name => "Closed";
+    public void PressButton(Door door) => door.SetState(new OpenState());
+}
+
+public sealed class LockedState : IDoorState
+{
+ public string Name => "Locked";
+ public void PressButton(Door door)
+ {
+ // brak reakcji
+ }
+}
+
+internal class Program
+{
+ static void Main()
+ {
+ var door = new Door(new ClosedState());
+ Console.WriteLine(door.StateName);
+
+ door.PressButton(); // Closed -> Open
+ Console.WriteLine(door.StateName);
+
+ door.PressButton(); // Open -> Closed
+ Console.WriteLine(door.StateName);
+
+ door.SetState(new LockedState());
+ door.PressButton(); // Locked -> brak reakcji
+ Console.WriteLine(door.StateName);
+ }
+}
