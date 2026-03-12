331bc4b5c4026757b7142cc75fab5303404d42fd
RadomirSz <114557710+RadomirSz@users.noreply.github.com>
2026-03-12 12:20:43 +0100

    Add files via upload

---
+++ Programowanie Aplikacji Desktopowych/klasa 4/DrogaWiadomosci_12-03/DrogaWiadomosci_12-03/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace DrogaWiadomosci_12_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kronikarz wiesiek = new KronikarzKonsolowy();
+            Archiwista chiefkeef = new ArchiwistaCachowy();
+
+            ObsłużycielWiadomości sekretarkaAsia = new ObsłużycielWiadomości(znajomyKronikarz: wiesiek, znajomyArchiwista: chiefkeef);
+
+            Console.WriteLine("Przekaz wiadomość do pani Asi:");
+            string wiadomosc = Console.ReadLine();
+            sekretarkaAsia.obsłużWiadomość(wiadomosc);
+        }
+    }
+
+    public class ObsłużycielWiadomości(Kronikarz znajomyKronikarz, Archiwista znajomyArchiwista)
+    {
+        public void obsłużWiadomość(string wiadomosc)
+        {
+            znajomyKronikarz.zapisz(wiadomosc);
+            znajomyArchiwista.zapisz(wiadomosc);
+        }
+    }
+
+    public interface Kronikarz
+    {
+        void zapisz(string wiadomosc);
+    }
+    class KronikarzKonsolowy : Kronikarz
+    {
+        public void zapisz(string wiadomosc)
+        {
+            Console.WriteLine(wiadomosc);
+        }
+    }
+    class KronikarzPlikowy : Kronikarz
+    {
+        public void zapisz(string wiadomosc)
+        {
+            Console.WriteLine($"zapisuję '{wiadomosc}' do pliku");
+        }
+    }
+
+    public interface Archiwista
+    {
+        void zapisz(string wiadomosc);
+    }
+
+    public class ArchiwistaBazodanowy : Archiwista
+    {
+        public void zapisz(string wiadomosc)
+        {
+            Console.WriteLine($"zapisalem '{wiadomosc}' do bazy danych");
+        }
+    }
+
+    public class ArchiwistaCachowy : Archiwista
+    {
+        public void zapisz(string wiadomosc)
+        {
+            Console.WriteLine($"dodałem '{wiadomosc}' do pamieci podrecznej");
+        }
+    }
+}
